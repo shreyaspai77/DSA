b4addb74f22e7baf22419800b8cd40b05d6329fa
shreyaspai77 <shreyaspai25@gmail.com>
2024-02-07 00:12:07 +0530

    small changes made

--- arrays/largestElement.java
+++ arrays/largestElement.java
@@ -1,15 +1,16 @@
 public class largestElement {
-    public static void largestElement(int[] arr) {
-        int max=arr[0];
-        for(int i:arr){
-            if(i>max){
-                max=i;
+    public static void largestEle(int[] arr) {
+        int max = arr[0];
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
             }
         }
         System.out.println(max);
     }
+
     public static void main(String[] args) {
-        int [] arr= {1,2,3,4,5};
-        largestElement(arr);
+        int[] arr = { 1, 2, 3, 4, 5 };
+        largestEle(arr);
     }
 }
