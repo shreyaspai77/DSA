02f41c8c285c127e8f6a7b673800c930d0b07014
shreyaspai77 <shreyaspai25@gmail.com>
2024-02-13 19:34:34 +0530

    majorityElement

---
+++ arrays/majorityElement.java
@@ -0,0 +1,26 @@
+//169. Majority Element
+
+public class majorityElement {
+    public static int majorityElement(int[] nums) {
+        int count = 1;
+        int contestant = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                contestant = nums[i];
+            }
+            if (nums[i] == contestant) {
+                count++;
+            } else {
+                count--;
+            }
+
+        }
+        return contestant;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1, 1, 2, 3, 4 };
+        int freq = majorityElement(nums);
+        System.out.println(freq);
+    }
+}
