f0e000807d68c835058a2747d8a379b001276d30
shreyaspai77 <shreyaspai25@gmail.com>
2024-02-13 19:35:01 +0530

    basics of hashmaps and sets in java

--- basics/setHash.java
+++ basics/setHash.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class setHash {
     public static void main(String[] args) {
         Map<Character, Integer> intStr = new HashMap<>();
+        ArrayList<Integer> arr = new ArrayList<>();
         String name = "shreyas";
         char[] namearr = name.toCharArray();
         for (char c : namearr) {
@@ -13,6 +15,13 @@ public class setHash {
             if (entry.getValue() == 1) {
                 System.out.println(entry.getKey());
             }
+        }
+        arr.add(1);
+        arr.add(7);
+        arr.add(0);
+        arr.sort(null);
+        for (Integer c : arr) {
+            System.out.println(c);
         }
 
     }
